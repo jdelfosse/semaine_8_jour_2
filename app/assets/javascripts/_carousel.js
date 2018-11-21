fa3a758095459da18487ebdc79be14cbf073e68e
jerome.delfosse <jerome.delfosse.78@gmail.com>
2018-11-21 01:57:18 +0100

    caroussel

---
+++ app/assets/javascripts/_carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function() { // lorsque le DOM est chargé intégralement
+
+
+  // switch carousel image every 2 seconds
+  $('.carousel').carousel({
+    interval: 5000
+  })
+
+  // display carousel when user click on button below the images
+
+  // Failed
+
+
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,47 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-} 
-
-function openTitle(evt, Title) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(Title).style.display = "block";
-    evt.currentTarget.className += " active";
-} 
 
